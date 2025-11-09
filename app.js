42f9acf580f9eb6aaa2ceb5fb7c4a6cf9e4000c1
Daniel Bingham <danielbingham000@gmail.com>
2025-11-09 17:14:37 -0500

    "test"

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 
 let url = "superheroes.php";
+//document.getElementById("result").innerHTML="i got tet";
 function thisFunc(){
     console.log("we plan to run a php file here by clicking a button");
     const xhttp = new XMLHttpRequest();
     xhttp.open("GET", url, true);
     xhttp.onload = function checkStatus() {
         if (this.status === 200) {
-           window.alert(this.responseText);
+           document.getElementById("result").textContent=this.responseText;
         }
         else {
             window.alert("we got an Error: " + this.status);
